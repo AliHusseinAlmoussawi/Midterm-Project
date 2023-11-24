12c0c94372ef0a838b228ab1a15f6140ff628765
user <moussawiali2002@gmail.com>
2023-11-24 18:51:54 +0200

    Creating greeting page

---
+++ main.py
@@ -0,0 +1,18 @@
+#Greeting the user and displaying options
+def mainPage():
+    print("Hello, choose an option:")
+    print("1. Open Tab")
+    print("2. Close Tab")
+    print("3. Switch Tab")
+    print("4. Display All Tabs")
+    print("5. Open Nested Tab")
+    print("6. Sort All Tabs")
+    print("7. Save Tabs")
+    print("8. Import Tabs")
+    print("9. Exit")
+
+
+if __name__ == '__main__':
+    mainPage()
+
+
